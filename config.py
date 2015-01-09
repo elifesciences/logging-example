727b7243bf0e83fb14d632f841bbb4c2dc84d12e
Luke Skibinski <l.skibinski@elifesciences.org>
2015-01-09 15:27:17 +0000

    removed some old debug statements

--- config.py
+++ config.py
@@ -12,27 +12,22 @@ DEV_FORMAT = logging.Formatter("%(created)f - %(levelname)s - %(funcName)s:%(lin
 LOGFILE = "./example.log"
 DEV_LOGFILE = "example.dev.log"
 
-#
-# example
-#
-
-# setup some basic logging
 # http://docs.python.org/2/howto/logging-cookbook.html
 logger = logging.getLogger("") # important! this is the *root logger* 
                                # all other loggers are derived from this one
 logger.setLevel(logging.DEBUG) # *default* output level
 
-# good for debugging
-h = logging.StreamHandler() # defaults to stderr
+# StreamHandler sends to stderr by default
+h = logging.StreamHandler()
 h.setLevel(logging.INFO) # output level for *this handler*
 h.setFormatter(FORMAT)
 
-# better for stable code in production
+# FileHandler sends to a named file
 h2 = logging.FileHandler(LOGFILE)
 h2.setLevel(logging.WARN) # change to INFO if code is less-than-stable
 h2.setFormatter(FORMAT)
 
-# outputs *everything* to a seperate file, good for dev
+# outputs *everything* to a seperate file, good for debugging during dev
 h3 = logging.FileHandler(DEV_LOGFILE)
 # h3.setLevel(logging.DEBUG) # not neccessary as the root logger outputs at DEBUG
 h3.setFormatter(DEV_FORMAT) # for dev you might want a friendlier or more indepth logging format
