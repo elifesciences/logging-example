d00e2716b241f8bc36b2dd1e5ed13d8ce382457c
Luke Skibinski <l.skibinski@elifesciences.org>
2015-08-13 17:04:14 +0100

    cleaned up the php logger a little

--- php/logging.php
+++ php/logging.php
@@ -1,20 +1,24 @@
 <?php
 
+# do `composer require monolog/monolog` to install
+
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Formatter\JsonFormatter;
 
+# reset error handling for this script
 error_reporting(-1);
 ini_set('display_errors', 1);
 
 require_once('vendor/autoload.php');
+
 
 function el_format_record(array $record) {
     return array(
         "timestamp" => date_timestamp_get($record['datetime']),
         "log_level" => $record['level_name'],
         "process" => $record['channel'],
-        "section" => __FILE__,
+        "section" => __FILE__, # pretty sure this won't work as I expect it to
         "message" => $record['message'],
         "context" => $record['context'],
     );
@@ -31,18 +35,15 @@ class eLifeJsonFormatter extends Monolog\Formatter\JsonFormatter {
     }
 }
 
-// create a log channel
 $log = new Logger('example-app');
 $formatter = new eLifeJsonFormatter();
 $stream = new StreamHandler('/tmp/monolog-test.log', Logger::DEBUG);
 $stream->setFormatter($formatter);
 $log->pushHandler($stream);
 
-
 //
 //
 //
 
-// add records to the log
 $log->addWarning('this is a warning');
 $log->addError('this is an error');
